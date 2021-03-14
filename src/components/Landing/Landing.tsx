ebb353b8c7423780c9b3ecaa1be5dffbee5382f4
2001-otter <nicolastheodarus@gmail.com>
2021-03-14 12:14:19 +0100

    styling update

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -6,7 +6,12 @@ const Landing = (props: any) => {
     return(
        <div className={styles.Landing}>
            <div className={styles.TopHalf}>
-                <text>Find the Webinar you need.</text>
+                <strong>Find the Webinar you need.</strong>
+                <p>
+                    Now you can FIND the Webinar,<br/>
+                    that suits you!
+                </p>
+                <button>Find your webinar now!</button>
            </div>
            <div className={styles.BottomHalf}>
 
